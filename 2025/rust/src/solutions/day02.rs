dbaf2f71b03665c5d65c5bae758d033b0ec03786
ErrorNoInternet <errornointernet@envs.net>
2025-12-02 01:43:47 -0500

    2025/d02/rust: optimize solution

--- 2025/rust/src/solutions/day02.rs
+++ 2025/rust/src/solutions/day02.rs
@@ -1,56 +1,52 @@
 pub struct Day02;
 
-fn is_valid_part1_id(id: i64) -> bool {
-    let len = (id as f64).log10().ceil() as i32;
-    if len % 2 != 0 {
-        return true;
-    }
-    let divisor = 10f32.powi(len / 2) as i64;
-    let half = id / divisor;
-    id != (half * divisor + half)
+fn parse(input: &str) -> Vec<(u64, u64)> {
+    input
+        .split(",")
+        .map(|range| {
+            let (left, right) = range.trim().split_once("-").unwrap();
+            (left.parse().unwrap(), right.parse().unwrap())
+        })
+        .collect()
 }
 
-fn is_valid_part2_id(id: i64) -> bool {
-    let id = id.to_string();
-    for subset_len in (1..=id.len() / 2).rev() {
-        let repititions = id.len() / subset_len;
-        if !id.len().is_multiple_of(repititions) {
-            continue;
+fn sum(ranges: &[(u64, u64)], digits: &[u32], lengths: &[u32]) -> u64 {
+    let mut result = 0;
+
+    for (d, l) in digits.iter().zip(lengths) {
+        let repetitions = d / l;
+        let power = 10_u64.pow(*l);
+        let mut step = 0;
+        for _ in 0..repetitions {
+            step = step * power + 1;
         }
-        if id == id[0..subset_len].repeat(repititions) {
-            return false;
+        let invalid_start = step * (power / 10);
+        let invalid_end = step * (power - 1);
+
+        for &(start, end) in ranges {
+            let lower = start.next_multiple_of(step).max(invalid_start);
+            let upper = end.min(invalid_end);
+            if lower <= upper {
+                let n = (upper - lower) / step;
+                let m = n * (n + 1) / 2;
+                result += lower * (n + 1) + step * m;
+            }
         }
     }
-    true
+
+    result
 }
 
 impl super::Solution for Day02 {
     fn part1(&self, input: &str) -> String {
-        let mut sum = 0;
-        for segment in input.split(",") {
-            let (first, last) = segment.trim().split_once("-").unwrap();
-            let (first_id, last_id) = (first.parse().unwrap(), last.parse().unwrap());
-            for id in first_id..=last_id {
-                if !is_valid_part1_id(id) {
-                    sum += id;
-                }
-            }
-        }
-        sum.to_string()
+        sum(&parse(input), &[2, 4, 6, 8, 10], &[1, 2, 3, 4, 5]).to_string()
     }
 
     fn part2(&self, input: &str) -> String {
-        let mut sum = 0;
-        for segment in input.split(",") {
-            let (first, last) = segment.trim().split_once("-").unwrap();
-            let (first_id, last_id) = (first.parse().unwrap(), last.parse().unwrap());
-            for id in first_id..=last_id {
-                if !is_valid_part2_id(id) {
-                    sum += id;
-                }
-            }
-        }
-        sum.to_string()
+        let result = sum(&parse(input), &[2, 4, 6, 8, 10], &[1, 2, 3, 4, 5])
+            + sum(&parse(input), &[3, 5, 6, 7, 9, 10], &[1, 1, 2, 1, 3, 2])
+            - sum(&parse(input), &[6, 10], &[1, 1]);
+        result.to_string()
     }
 }
 
